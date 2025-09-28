cff3215e56035fb042cefc14fe66caf6cddcd4a5
angelmicelti <angelmicelti@users.noreply.github.com>
2025-09-28 21:57:04 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// Example service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        // Add other files to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
