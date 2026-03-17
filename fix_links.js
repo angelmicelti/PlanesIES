a12d8002fab16c5058fbf0f8d0f740e15c96bf24
angelmicelti <angelmicelti@users.noreply.github.com>
2026-03-17 17:38:18 +0100

    Revert "Reapply "Create fix_links.js""
    This reverts commit cfd8852cf9f15509700338de89d535217932ee6a.

--- fix_links.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const filename = 'CronogramaLector.html';
-let content = fs.readFileSync(filename, 'utf8');
-content = content.replace(/docs\.google\.com\/spreadsheets\/d\//g, 'docs.google.com/spreadsheets/u/0/d/');
-fs.writeFileSync(filename, content, 'utf8');
-console.log('Fixed CronogramaLector.html');
